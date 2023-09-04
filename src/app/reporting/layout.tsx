f85564330daa77653925f04eb39eb361b66c1e52
RicBruD4S <132678601+RicBruD4S@users.noreply.github.com>
2023-09-04 17:27:40 +0200

    Update layout.tsx

--- src/app/reporting/layout.tsx
+++ src/app/reporting/layout.tsx
@@ -1,6 +1,8 @@
 import { ProtectedPage } from "@/features/auth/protected-page";
 import { MainMenu } from "@/features/menu/menu";
 import { AI_NAME } from "@/features/theme/customise";
+import Link from "next/link";
+import { UserProfile } from "../user-profile"
 
 export const metadata = {
   title: AI_NAME,
@@ -14,7 +16,22 @@ export default async function RootLayout({
 }) {
   return (
     <ProtectedPage>
-      <MainMenu />
+<div className="flex gap-2 flex-col justify-between">
+      <div className="flex gap-2 flex-col justify-between">
+        <Link
+          href="/"
+          className="w-10 h-10 items-center justify-center flex"
+          title="Home"
+        >
+        </Link>
+        <Link
+          href="/reporting"
+          className="w-10 h-10 items-center justify-center flex rounded-full hover:bg-secondary"
+          title="Reporting"
+        >
+        </Link>
+      </div>
+    </div>
       <div className="flex-1">{children}</div>
     </ProtectedPage>
   );
