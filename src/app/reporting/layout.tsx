256b0188133091ffb6d99ac35dceecf3c6141b02
RicBruD4S <132678601+RicBruD4S@users.noreply.github.com>
2023-09-04 15:58:27 +0200

    Update layout.tsx

--- src/app/reporting/layout.tsx
+++ src/app/reporting/layout.tsx
@@ -1,26 +1,20 @@
-"use client"
 import { ProtectedPage } from "@/features/auth/protected-page";
 import { MainMenu } from "@/features/menu/menu";
 import { AI_NAME } from "@/features/theme/customise";
-import React, { useState } from "react";
 
 export const metadata = {
   title: AI_NAME,
   description: AI_NAME,
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // State to manage the isOpen prop
-  const [ isOpen, setIsOpen ] = useState(false);
-
   return (
     <ProtectedPage>
-      <MainMenu isOpen={isOpen} setIsOpen={setIsOpen}>
-      </MainMenu>
+      <MainMenu />
       <div className="flex-1">{children}</div>
     </ProtectedPage>
   );
